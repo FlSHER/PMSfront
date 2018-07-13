f6590026dd19920736a87dc3f0a2b57f2aafa3e6
gui <374779789@qq.com>
2018-07-13 18:48:10 +0800

    内容布局

--- PC/src/layouts/BasicLayout.js
+++ PC/src/layouts/BasicLayout.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Layout, Icon } from 'antd';
+import { Layout, Icon, Breadcrumb, Menu } from 'antd';
 import DocumentTitle from 'react-document-title';
 import { connect } from 'dva';
 import { Route, Redirect, Switch, routerRedux } from 'dva/router';
 import { ContainerQuery } from 'react-container-query';
 import classNames from 'classnames';
-import { enquireScreen } from 'enquire-js';
-import GlobalHeader from '../components/GlobalHeader';
+// import { enquireScreen } from 'enquire-js';
+import GlobalHeader from '../components/PMSGlobalHeader';
 import GlobalFooter from '../components/GlobalFooter';
-import SiderMenu from '../components/SiderMenu';
 import NotFound from '../routes/Exception/404';
 import { getRoutes } from '../utils/utils';
 import Authorized from '../utils/Authorized';
@@ -17,7 +16,7 @@ import { getMenuData } from '../common/menu';
 import logo from '../assets/logo.svg';
 import './BasiclLayout.less';
 
-const { Content } = Layout;
+const { Content, Sider } = Layout;
 const { AuthorizedRoute } = Authorized;
 
 /**
@@ -60,19 +59,19 @@ const query = {
   },
 };
 
-let isMobile;
-enquireScreen((b) => {
-  isMobile = b;
-});
+// let isMobile;
+// enquireScreen((b) => {
+//   isMobile = b;
+// });
 
 class BasicLayout extends React.PureComponent {
   static childContextTypes = {
     location: PropTypes.object,
     breadcrumbNameMap: PropTypes.object,
   }
-  state = {
-    isMobile,
-  };
+  // state = {
+  //   isMobile,
+  // };
 
   getChildContext() {
     const { location, routerData } = this.props;
@@ -89,11 +88,11 @@ class BasicLayout extends React.PureComponent {
   componentDidMount() {
     this.checkOauthPermission();
     // setInterval(this.checkOauthPermission, 1000);
-    enquireScreen((mobile) => {
-      this.setState({
-        isMobile: mobile,
-      });
-    });
+    // enquireScreen((mobile) => {
+    //   this.setState({
+    //     isMobile: mobile,
+    //   });
+    // });
   }
 
   getPageTitle() {
@@ -110,34 +109,6 @@ class BasicLayout extends React.PureComponent {
     this.props.dispatch({
       type: 'currentUser/fetchCurrent',
     });
-  }
-
-  handleMenuCollapse = (collapsed) => {
-    this.props.dispatch({
-      type: 'global/changeLayoutCollapsed',
-      payload: collapsed,
-    });
-  }
-  handleNoticeClear = (type) => {
-    message.success(`清空了${type}`);
-    this.props.dispatch({
-      type: 'global/clearNotices',
-      payload: type,
-    });
-  }
-  handleMenuClick = ({ key }) => {
-    if (key === 'logout') {
-      this.props.dispatch({
-        type: 'login/logout',
-      });
-    }
-  }
-  handleNoticeVisibleChange = (visible) => {
-    if (visible) {
-      this.props.dispatch({
-        type: 'global/fetchNotices',
-      });
-    }
   }
 
   checkOauthPermission() {
@@ -159,84 +130,63 @@ class BasicLayout extends React.PureComponent {
 
   render() {
     const {
-      currentUser, routerData, match, location, collapsed, notices,
+      currentUser, routerData, match, location,
     } = this.props;
     const layout = (
       <Layout>
-        <SiderMenu
-          logo={logo}
-          // 不带Authorized参数的情况下如果没有权限,会强制跳到403界面
-          // If you do not have the Authorized parameter
-          // you will be forced to jump to the 403 interface without permission
-          Authorized={Authorized}
-          menuData={getMenuData()}
-          collapsed={collapsed}
-          location={location}
-          isMobile={this.state.isMobile}
-          onCollapse={this.handleMenuCollapse}
-        />
         <Layout>
           <GlobalHeader
             logo={logo}
             currentUser={currentUser}
-            // fetchingNotices={fetchingNotices}
-            notices={notices}
-            collapsed={collapsed}
-            isMobile={this.state.isMobile}
-            onNoticeClear={this.handleNoticeClear}
-            onCollapse={this.handleMenuCollapse}
-            onMenuClick={this.handleMenuClick}
-            onNoticeVisibleChange={this.handleNoticeVisibleChange}
+            location={location}
+            menuData={getMenuData()}
           />
-          <Content style={{ margin: '24px 24px 0', height: '100%' }}>
-            <div style={{ minHeight: 'calc(100vh - 260px)' }}>
-              <Switch>
-                {
-                  getRoutes(match.path, routerData).map(item =>
-                    (
-                      <AuthorizedRoute
-                        key={item.key}
-                        path={item.path}
-                        component={item.component}
-                        exact={item.exact}
-                        authority={item.authority}
-                        redirectPath="/exception/403"
-                      />
-                    )
-                  )
-                }
-                {
-                  redirectData.map(item =>
-                    <Redirect key={item.from} exact from={item.from} to={item.to} />
-                  )
-                }
-                <Redirect exact from="/" to="/dashboard/analysis" />
-                <Route render={NotFound} />
-              </Switch>
-            </div>
-            <GlobalFooter
-              // links={[{
-              //   key: 'Pro 首页',
-              //   title: 'Pro 首页',
-              //   href: 'http://pro.ant.design',
-              //   blankTarget: true,
-              // }, {
-              //   key: 'github',
-              //   title: <Icon type="github" />,
-              //   href: 'https://github.com/ant-design/ant-design-pro',
-              //   blankTarget: true,
-              // }, {
-              //   key: 'Ant Design',
-              //   title: 'Ant Design',
-              //   href: 'http://ant.design',
-              //   blankTarget: true,
-              // }]}
-              copyright={
-                <div>
-                  Copyright <Icon type="copyright" /> 2018 喜歌实业IT部出品
+          <Content className="container">
+            <Breadcrumb className="creadcrumb">
+              <Breadcrumb.Item>奖扣</Breadcrumb.Item>
+              <Breadcrumb.Item>记录奖扣</Breadcrumb.Item>
+              <Breadcrumb.Item>新建奖扣</Breadcrumb.Item>
+            </Breadcrumb>
+            <Layout className="content">
+              <Sider width={200} style={{ background: '#fff' }}>
+                <Menu
+                  mode="inline"
+                  className="ant-left-menu"
+                  defaultSelectedKeys={['1']}
+                  defaultOpenKeys={['sub1']}
+                >
+                  <Menu.Item key="1">记录奖扣</Menu.Item>
+                  <Menu.Item key="2">我的奖扣</Menu.Item>
+                </Menu>
+              </Sider>
+              <Content style={{ background: '#ffffff', marginLeft: '-20px' }}>
+                <div style={{ minHeight: 'calc(100vh - 260px)' }}>
+                  <Switch>
+                    {
+                      getRoutes(match.path, routerData).map(item =>
+                        (
+                          <AuthorizedRoute
+                            key={item.key}
+                            path={item.path}
+                            component={item.component}
+                            exact={item.exact}
+                            authority={item.authority}
+                            redirectPath="/exception/403"
+                          />
+                        )
+                      )
+                    }
+                    {
+                      redirectData.map(item =>
+                        <Redirect key={item.from} exact from={item.from} to={item.to} />
+                      )
+                    }
+                    <Route render={NotFound} />
+                  </Switch>
                 </div>
-              }
-            />
+                <GlobalFooter copyright={<React.Fragment> Copyright <Icon type="copyright" /> 2018 喜歌实业IT部出品</React.Fragment>} />
+              </Content>
+            </Layout>
           </Content>
         </Layout>
       </Layout>
