264a8d8789fa062e798d777bf91ea9fee24cbd74
gui <374779789@qq.com>
2018-08-29 15:34:55 +0800

    dompl: 当月修改，单月积分

--- PC/src/routes/Point/my/index.js
+++ PC/src/routes/Point/my/index.js
@@ -17,7 +17,7 @@ export default class extends React.PureComponent {
     return (
       <div className={styles.tabs}>
         <Tabs defaultActiveKey="1">
-          <TabPane key="1" tab="当月积分" forceRender style={style}>
+          <TabPane key="1" tab="单月积分" forceRender style={style}>
             <ThisMonth staffSn={staffSn} datetime={datetime} />
           </TabPane>
           <TabPane tab="累计积分" key="2" forceRender style={style}>
