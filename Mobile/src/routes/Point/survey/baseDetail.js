4c96d211dc92a4a0122f7538b01b70f32b00892d
weiyingHappy <949736522@qq.com>
2018-09-05 09:13:22 +0800

    基础分详情

--- Mobile/src/routes/Point/survey/baseDetail.js
+++ Mobile/src/routes/Point/survey/baseDetail.js
@@ -24,61 +24,81 @@ export default class RankingGroup extends React.Component {
     });
   }
 
-  render() {
-    const { baseDetails = {} } = this.props;
-    const { foreignKey } = this.state;
-    const detail = baseDetails[`${foreignKey}`] || {};
-    const { details } = detail;
-    const uerInfo = {
-      realname: detail.staff_name,
-      staff_sn: detail.staff_sn,
-      department: {
-        full_name: detail.department_name,
-      },
-      brand: {
-        name: detail.brand_name,
-      },
-    };
-    if (!details) { return null; }
-    const [maxPoint, position, certificate] = details;
-    return (
-      <div
-        className={styles.con}
-        direction="column"
-      >
-        <div className={styles.con_content}>
-          <WhiteSpace size="md" />
-          <WingBlank>
-            <PersonInfo info={uerInfo} />
-          </WingBlank>
-          <WhiteSpace size="md" />
-          <WingBlank>
-            <div className={style.base_point}>
-              <span>基础分</span>
-              <span>{detail.point_b}</span>
-            </div>
-          </WingBlank>
-          <WhiteSpace size="md" />
-          <WingBlank>
-            <p className={style.construct}>积分构成</p>
-          </WingBlank>
-          <WingBlank>
-            <div className={style.card}>
-              <div className={style.card_title}>
-                <span>工龄分</span>
-                <span>{maxPoint.point}</span>
-              </div>
-              <div className={style.card_content}>
-                <div className={style.item}>
-                  <div className={style.item_title}>
-                    <span>工龄:{maxPoint.data['工龄']}</span>
+getPointType = (details) => {
+  const types = ['max_point', 'position', 'certificate'];
+  const cur = [];
+  types.forEach((type) => {
+    const [curType] = details.filter(item => item.type === type);
+    cur.push(curType);
+  });
+  return cur;
+}
+
+render() {
+  const { baseDetails = {} } = this.props;
+  const { foreignKey } = this.state;
+  const detail = baseDetails[`${foreignKey}`] || {};
+  const { details } = detail;
+  const uerInfo = {
+    realname: detail.staff_name,
+    staff_sn: detail.staff_sn,
+    department: {
+      full_name: detail.department_name,
+    },
+    brand: {
+      name: detail.brand_name,
+    },
+  };
+  if (!details) { return null; }
+  const [maxPoint, position, certificate] = this.getPointType(details);
+  // const [maxPoint] = details.filter(item => item.type === 'position');
+  // const [maxPoint] = details.filter(item => item.type === 'position');
+  // const [maxPoint] = details.filter(item => item.type === 'position');
+
+  return (
+    <div
+      className={styles.con}
+      direction="column"
+    >
+      <div className={styles.con_content}>
+        <WhiteSpace size="md" />
+        <WingBlank>
+          <PersonInfo info={uerInfo} />
+        </WingBlank>
+        <WhiteSpace size="md" />
+        <WingBlank>
+          <div className={style.base_point}>
+            <span>基础分</span>
+            <span>{detail.point_b}</span>
+          </div>
+        </WingBlank>
+        <WhiteSpace size="md" />
+        <WingBlank>
+          <p className={style.construct}>积分构成</p>
+        </WingBlank>
+        {maxPoint && (
+          <React.Fragment>
+            <WingBlank>
+              <div className={style.card}>
+                <div className={style.card_title}>
+                  <span>工龄分</span>
+                  <span>{maxPoint.point}</span>
+                </div>
+                <div className={style.card_content}>
+                  <div className={style.item}>
+                    <div className={style.item_title}>
+                      <span>工龄:{maxPoint.data['工龄']}</span>
+                    </div>
+                    <span className={style.remark}>入职时间:{maxPoint.data['入职时间']}</span>
                   </div>
-                  <span className={style.remark}>入职时间:{maxPoint.data['入职时间']}</span>
                 </div>
               </div>
-            </div>
-          </WingBlank>
-          <WhiteSpace size="md" />
+            </WingBlank>
+            <WhiteSpace size="md" />
+          </React.Fragment>
+          )}
+        {position && (
+        <React.Fragment>
           <WingBlank>
             <div className={style.card}>
               <div className={style.card_title}>
@@ -95,6 +115,9 @@ export default class RankingGroup extends React.Component {
             </div>
           </WingBlank>
           <WhiteSpace size="md" />
+        </React.Fragment>
+        )}
+        {certificate && (
           <WingBlank>
             <div className={style.card}>
               <div className={style.card_title}>
@@ -103,22 +126,25 @@ export default class RankingGroup extends React.Component {
               </div>
               <div className={style.card_content}>
                 {certificate.data.map((item, i) => {
-                const idx = i;
-                return (
-                  <div className={style.item} key={idx}>
-                    <div className={style.item_title}>
-                      <span>{item.name}</span>
-                      <span>{item.point}</span>
-                    </div>
-                    <span className={style.remark}>{item.description || '暂无'}</span>
-                  </div>
-                );
-              })}
+        const idx = i;
+        return (
+          <div className={style.item} key={idx}>
+            <div className={style.item_title}>
+              <span>{item.name}</span>
+              <span>{item.point}</span>
+            </div>
+            <span className={style.remark}>{item.description || '暂无'}</span>
+          </div>
+        );
+      })}
               </div>
             </div>
           </WingBlank>
-        </div>
+
+)}
+
       </div>
-    );
-  }
+    </div>
+  );
+}
 }
