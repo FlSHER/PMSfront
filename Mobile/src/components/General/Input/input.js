1a5e6a0e7a7a09db7aaace2e2415be59831ede2e
weiyingHappy <949736522@qq.com>
2018-10-23 14:46:42 +0800

    数字

--- Mobile/src/components/General/Input/input.js
+++ Mobile/src/components/General/Input/input.js
@@ -2,26 +2,73 @@ import React from 'react';
 import './index.less';
 
 class Input extends React.Component {
-  makeInputValue = (newValue) => {
-    let value = newValue;
-    const { range, onChange } = this.props;
-    if (range) {
-      if (parseFloat(value) < range.min) {
-        value = range.min;
-      }
-      if (parseFloat(value) > range.max) {
-        value = range.max;
-      }
+  constructor(props) {
+    const { value } = props;
+    super(props);
+    this.state = {
+      value,
+    };
+  }
+  componentWillReceiveProps(props) {
+    const { value } = props;
+    if (value !== this.props.value) {
+      this.setState({
+        value,
+      });
     }
+  }
+  makeInputValue = (newValue) => {
+    // let value = newValue;
+    const { onChange } = this.props;
+    // if (range) {
+    //   if (parseFloat(value) < range.min) {
+    //     value = range.min;
+    //   }
+    //   if (parseFloat(value) > range.max) {
+    //     value = range.max;
+    //   }
+    // }
     // const numberValue = parseFloat(value);
     // if (Math.floor(numberValue) === numberValue) {
     //   value = Number(value);
     // }
-    onChange(value);
+    this.setState({
+      value: newValue,
+    });
+    onChange(newValue);
     // return value;
   }
+
+  formatIntValue = (v, scale, min, max) => {
+    // const { scale, min } = field;
+    // const value = (v !== '' && min !== '' && (min - v > 0)) ? min : (v - max > 0 ? max : v);
+    console.log(scale);
+    let value = v;
+    if (v !== '') {
+      if (min !== '' && min - v > 0) {
+        value = min;
+      }
+      if (max !== '' && v - max > 0) {
+        value = max;
+      }
+    }
+    const idx = value.indexOf('.');
+    const curScale = idx > -1 ? value.slice(idx + 1).length : 0;
+    // const newValue = curScale > scale ? (value.slice(0, value.indexOf('.') + (scale - 0) + 1))
+    // : Number(value).toFixed(scale);
+    let newValue;
+    if (v !== '' && !isNaN(v)) {
+      const tmpValue = `${Number(value)}`;
+      newValue = curScale > scale ? (tmpValue.slice(0, tmpValue.indexOf('.') + (scale - 0) + 1)) : Number(value).toFixed(scale);
+    } else {
+      newValue = '';
+    }
+    return newValue;
+  }
   render() {
-    const { value, onChange, type, style, floatNumber } = this.props;
+    const { onChange, type, style, floatNumber, range } = this.props;
+    const { value } = this.state;
+    console.log(floatNumber);
     return (
       <input
         value={`${value}`}
@@ -35,13 +82,26 @@ class Input extends React.Component {
         onBlur={() => {
           if (floatNumber) {
             const a = this.ptr.value;
-            const b = Number(a);
-            const c = b.toFixed(floatNumber);
-            let v = b;
-            if (Math.floor(c) === Number(c)) {
-              v = Number(c);
-            }
-            onChange(v);
+            // const b = Number(a);
+            // const c = b.toFixed(floatNumber);
+            // let v = b;
+            // console.log(v, c);
+            // if (Math.floor(c) === Number(c)) {
+            //   v = Number(c);
+            // }
+            // if (range) {
+            //   if (parseFloat(v) - range.min < 0) {
+            //     v = range.min;
+            //   }
+            //   if (parseFloat(v) - range.max > 0) {
+            //     v = range.max;
+            //   }
+            // }
+           const newValue = this.formatIntValue(a, floatNumber, range.min, range.max);
+           this.setState({
+             value: newValue,
+           });
+            onChange(newValue);
           }
         }
         }
@@ -55,6 +115,10 @@ Input.defaultProps = {
   type: 'number',
   onBlur: () => {
 
+  },
+  value: '',
+  range: {
+    min: '', max: '',
   },
   floatNumber: 0,
 };
